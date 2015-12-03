77b25abb40f20840e0ed93ffc117c6fd8ff97c4b
rizakii <riza.alaudin@kii.com>
2015-12-03 11:09:31 +0900

    add wiireless-control

--- nodejs/echo/main.js
+++ nodejs/echo/main.js
@@ -3,6 +3,15 @@ var bleno = require('bleno');
 var BlenoPrimaryService = bleno.PrimaryService;
 
 var EchoCharacteristic = require('./characteristic');
+var wpa_supplicant = require('wireless-tools/wpa_supplicant');
+
+var options = {
+  interface: 'wlan0',
+  ssid: 'synclore_guest',
+  passphrase: '',
+  driver: 'wext'
+};
+
 
 console.log('bleno - echo');
 
@@ -15,23 +24,19 @@ bleno.on('stateChange', function(state) {
     bleno.stopAdvertising();
   }
 });
-var WiFiControl = require('wifi-control');
 
-  //  Initialize wifi-control package with verbose output
-  WiFiControl.init({
-    debug: true
-  });
-
-  //  Try scanning for access points:
-  WiFiControl.scanForWiFi( function(err, response) {
-    if (err) console.log(error);
-    console.log(response);
-  });
-  
 bleno.on('advertisingStart', function(error) {
   console.log('on -> advertisingStart: ' + (error ? 'error ' + error : 'success'));
 
   if (!error) {
+    wpa_supplicant.enable(options, function(err) {
+      // connected to the wireless network
+      if (!err) {
+        console.log(err);
+      }else{
+        console.log('connected');
+      }
+    });
     bleno.setServices([
       new BlenoPrimaryService({
         uuid: 'E20A39F473F54BC4A12F17D1AD666661',
